abb9d9b7fdf9424d621cc8b438bb5b5eb2b1574f
KrimiWajih <krimi.wajih@gmail.com>
2025-02-03 17:41:57 +0100

    CheckPointRecursive
    CheckPointRecursive

---
+++ CheckPointRecursion.js
@@ -0,0 +1,31 @@
+function palindrome(s) {
+  const sorted2 = s.split("").reverse().join("");
+  return s === sorted2;
+}
+
+///////////////////////////////////////////////////////
+
+function fib(n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+console.log(fib(2));
+
+////////////////////////////////////
+function puissance(n, m) {
+  if (m === 0) {
+    return 1;
+  }
+  if (m == 1) {
+    return n;
+  }
+  if (m < 0) {
+    return 1 / puissance(n, -m);
+  }
+  return n * puissance(n, m - 1);
+}
